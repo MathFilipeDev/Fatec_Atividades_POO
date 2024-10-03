ac1e640c54806911242bdd04407d7ea41331bbf0
Matheus Filipe <math.filipe23@gmail.com>
2024-10-03 16:39:39 -0300

    [feat] add_aula06

---
+++ AULA06-ENCAPSULAMENTO-POO-main/aula06-encapsulamento/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Aluno chico = new Aluno("Matheus", 123456, 23, "M", true);
+        //chico.setNome("Matheus");
+        //chico.setNome("Filipe");
+        System.out.println(chico.getNome());
+
+    }
+}
